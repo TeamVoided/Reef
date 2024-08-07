b44ad8e7b3d4fcd230f6e0a016958566118ca36c
theendercore <theendercore@gmail.com>
2024-08-07 20:09:39 +0300

    - updated deps & gradle

--- src/main/kotlin/org/teamvoided/reef/world/gen/configured_feature/MonsterRoomFeature.kt
+++ src/main/kotlin/org/teamvoided/reef/world/gen/configured_feature/MonsterRoomFeature.kt
@@ -17,7 +17,7 @@ import net.minecraft.world.gen.feature.util.FeatureContext
 import org.teamvoided.reef.Reef.log
 import org.teamvoided.reef.world.gen.configured_feature.config.MonsterRoomFeatureConfig
 
-@Suppress("DEPRECATION")
+@Suppress("DEPRECATION", "ComplexCondition", "MagicNumber")
 class MonsterRoomFeature(codec: Codec<MonsterRoomFeatureConfig>) :
     Feature<MonsterRoomFeatureConfig>(codec) {
     override fun place(context: FeatureContext<MonsterRoomFeatureConfig>): Boolean {
