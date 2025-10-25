639d7258ff3c33c0c75d36b12a69225ec011975b
TheEnderCore <theendercore@gmail.com>
2025-10-25 15:31:15 +0300

    .

--- src/main/kotlin/org/teamvoided/reef/data/gen/ReefData.kt
+++ src/main/kotlin/org/teamvoided/reef/data/gen/ReefData.kt
@@ -2,7 +2,6 @@ package org.teamvoided.reef.data.gen
 
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
-import org.teamvoided.reef.Reef.log
 
 class ReefData : DataGeneratorEntrypoint {
     override fun onInitializeDataGenerator(gen: FabricDataGenerator) {
