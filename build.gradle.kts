d3db987b4b954720b9e17c8a1583387161deed9b
TheEnderCore <theendercore@gmail.com>
2026-03-29 21:10:14 +0300

    template update & 21.11 start

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,15 +3,17 @@
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
+
 plugins {
-    alias(libs.plugins.fabric.loom)
     alias(libs.plugins.kotlin.jvm)
-    alias(libs.plugins.kotlinx.serialization)
     alias(libs.plugins.iridium)
     alias(libs.plugins.iridium.publish)
     alias(libs.plugins.iridium.upload)
+    alias(libs.plugins.fabric.loom)
 }
+
 repositories {
+    maven("https://maven.fabricmc.net/")
     maven("https://teamvoided.org/releases") { content { includeGroup("org.teamvoided") } }
     maven("https://teamvoided.org/snapshots") { content { includeGroup("org.teamvoided") } }
     maven("https://maven.fzzyhmstrs.me/") { name = "FzzyMaven"; content { includeGroup("me.fzzyhmstrs") } }
@@ -23,83 +25,126 @@ repositories {
         }
     }
     maven("https://api.modrinth.com/maven") { content { includeGroup("maven.modrinth") } }
+    mavenLocal()
     mavenCentral()
-}
-
-modSettings {
-    entrypoint("main", "org.teamvoided.reef.Reef::commonInit")
-//    entrypoint("client", "org.teamvoided.reef.Reef::clientInit")
-    entrypoint("fabric-datagen", "org.teamvoided.reef.data.gen.ReefData")
-
-    mixinFile("${modId()}.mixins.json")
-//    accessWidener("${modId()}.accesswidener")
 }
 
 dependencies {
     modImplementation(fileTree("libs"))
-    modImplementation(libs.modmenu)
 
+    minecraft(libs.minecraft)
+    mappings(loom.officialMojangMappings())
+
+    // Dependencies
+    modImplementation(libs.fabric.loader)
+    modImplementation(libs.fabric.api)
+    modImplementation(libs.fabric.kotlin)
     modImplementation(libs.lithostitched)
+    // Compatibility
+    // Runtime
+    modImplementation(libs.modmenu)
+    // Testing
+//    modImplementation(libs.creative.works)
+//    modImplementation(libs.imguimc)
 }
+
+val username = "vDev"
+val uuid = iridium.fetchUUID(username) // Dev & vDev will always be null
 
 loom {
     splitEnvironmentSourceSets()
+    accessWidenerPath.set(File("src/main/resources/${iridium.modId}.classtweaker"))
+
+    mods {
+        register(iridium.modId) {
+            sourceSet(sourceSets.main.get())
+            sourceSet(sourceSets.getByName("client"))
+        }
+    }
+
     runs {
-        create("DataGen") {
+        named("client") {
+            programArgs("--username", username)
+            uuid?.let { programArgs("--uuid", it) }
+        }
+
+        create("randomClient") {
             client()
+            runDir("run")
             ideConfigGenerated(true)
-            vmArg("-Dfabric-api.datagen")
-            vmArg("-Dfabric-api.datagen.output-dir=${file("src/main/generated")}")
-            vmArg("-Dfabric-api.datagen.modid=${modSettings.modId()}")
-            runDir("build/datagen")
         }
 
         create("TestWorld") {
             client()
-            ideConfigGenerated(true)
             runDir("run")
-            programArgs("--quickPlaySingleplayer", "test")
+            ideConfigGenerated(true)
+            programArgs("--quickPlaySingleplayer", "test", "--username", username)
+            uuid?.let { programArgs("--uuid", it) }
         }
     }
 }
 
-sourceSets["main"].resources.srcDir("src/main/generated")
+fabricApi {
+    configureDataGeneration {
+        client = true
+        createRunConfiguration = true
+        createSourceSet = true
+        addToResources = true
+        modId = iridium.modId + "_vdatagen"
+
+        strictValidation = false
+    }
+}
 
 tasks {
-    val targetJavaVersion = 21
+    val javaVersion = libs.versions.java.get()
     withType<JavaCompile> {
         options.encoding = "UTF-8"
-        options.release.set(targetJavaVersion)
+        options.release.set(javaVersion.toInt())
     }
 
-    withType<KotlinCompile> {
-        compilerOptions.jvmTarget = JvmTarget.JVM_21
+    withType<KotlinCompile>().all {
+        compilerOptions.jvmTarget = JvmTarget.fromTarget(javaVersion)
     }
 
     java {
-        toolchain.languageVersion.set(JavaLanguageVersion.of(JavaVersion.toVersion(targetJavaVersion).toString()))
+        toolchain.languageVersion.set(JavaLanguageVersion.of(JavaVersion.toVersion(javaVersion).toString()))
         withSourcesJar()
     }
 
+    sourceSets.forEach { set ->
+        named<ProcessResources>(set.processResourcesTaskName) {
+            var expandProps = iridium.props.toMutableMap()
+            iridium.appendLibsVersionProps(expandProps, File("libs.versions.toml"))
+            filesMatching(
+                listOf("pack.mcmeta", "fabric.mod.json", "META-INF/mods.toml", "META-INF/neoforge.mods.toml")
+            ) {
+                expand(expandProps)
+            }
+            inputs.properties(expandProps)
+        }
+    }
 }
 
 publishScript {
     releaseRepository("TeamVoided", "https://maven.teamvoided.org/releases")
-    publication(modSettings.modId(), false)
-    publishSources(true)
+    publication(iridium.modId, isSnapshot = false)
+    publishSources = true
 }
 
-uploadConfig {
-//    debugMode = true
-    modrinthId = "AqJVwCCS"
-    curseId = "1007845"
+uploadScript {
+    debugMode = false
 
-    changeLog = "- fixed z-lands bug with eroded pillar tag"
+    modrinthId = "id"
+    curseId = "0"
 
-    // FabricApi
-    modrinthDependency("P7dR8mSH", uploadConfig.REQUIRED)
-    curseDependency("fabric-api", uploadConfig.REQUIRED)
-    // Fabric Language Kotlin
-    modrinthDependency("Ha28R6CL", uploadConfig.REQUIRED)
-    curseDependency("fabric-language-kotlin", uploadConfig.REQUIRED)
+    changelog = File("changelog.md").readText()
+
+    version += libs.versions.minecraft.get()
+    versionName = "${iridium.modName()} ${iridium.modVersion}"
+    jarTask = tasks.remapJar.get()
+
+    dependency("P7dR8mSH", "fabric-api")
+    dependency("Ha28R6CL", "fabric-language-kotlin")
+    dependency("hYykXjDp", "fzzy-config")
 }
